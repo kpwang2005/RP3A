dae21d096496924b2bd40e6d1d7e8d5eeb115c7f
kpwang2005 <kpwang2005@gmail.com>
2019-02-18 00:28:34 +0800

    distance sensor

---
+++ python_lab/dist_sensor_test.py
@@ -0,0 +1,10 @@
+from gpiozero import DistanceSensor
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import sleep
+
+factory = PiGPIOFactory()
+
+sensor = DistanceSensor(echo=17, trigger=18, pin_factory=factory)
+while True:
+    print('Distance: ', sensor.distance * 100)
+    sleep(1)
