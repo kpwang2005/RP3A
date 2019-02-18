12f8bf92de90a7e8eafcfa2671ebfa6cad094d38
kpwang2005 <kpwang2005@gmail.com>
2019-02-18 23:42:59 +0800

    add LED blink

--- python_lab/dist_sensor_test.py
+++ python_lab/dist_sensor_test.py
@@ -1,10 +1,18 @@
 from gpiozero import DistanceSensor
+from gpiozero import PWMLED
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
 factory = PiGPIOFactory()
 
 sensor = DistanceSensor(echo=18, trigger=17, pin_factory=factory)
+
+led = PWMLED(13, pin_factory=factory)
+
 while True:
     print('Distance: ', sensor.distance * 100)
-    sleep(1)
+    if sensor.distance < 0.3:
+        led.blink(0.2, 0.2)
+    else:
+        led.off()
+    sleep(2)
