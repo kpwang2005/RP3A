da5618806b19fbb04fa7bb6c532d580e31dc2302
kpwang2005 <32416156+kpwang2005@users.noreply.github.com>
2019-02-18 09:39:01 +0800

    Update dist_sensor_test.py

--- python_lab/dist_sensor_test.py
+++ python_lab/dist_sensor_test.py
@@ -4,7 +4,7 @@ from time import sleep
 
 factory = PiGPIOFactory()
 
-sensor = DistanceSensor(echo=17, trigger=18, pin_factory=factory)
+sensor = DistanceSensor(echo=18, trigger=17, pin_factory=factory)
 while True:
     print('Distance: ', sensor.distance * 100)
     sleep(1)
