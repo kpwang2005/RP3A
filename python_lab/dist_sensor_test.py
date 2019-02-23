af6eb31f8906069ebe05525a4e4871d5f96b80ab
kpwang2005 <kpwang2005@gmail.com>
2019-02-23 20:31:32 +0800

    pijuice

--- python_lab/dist_sensor_test.py
+++ python_lab/dist_sensor_test.py
@@ -2,17 +2,21 @@ from gpiozero import DistanceSensor
 from gpiozero import PWMLED
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
+from pijuice import PiJuice
 
 factory = PiGPIOFactory()
 
 sensor = DistanceSensor(echo=18, trigger=17, pin_factory=factory)
 
-led = PWMLED(13, pin_factory=factory)
+#led = PWMLED(13, pin_factory=factory)
+pijuice = PiJuice(1, 0x14)
 
 while True:
     print('Distance: ', sensor.distance * 100)
     if sensor.distance < 0.3:
-        led.blink(0.2, 0.2)
+        #led.blink(0.2, 0.2)
+        pijuice.status.SetLedBlink('D2', 2,[100, 0, 0], 500, [0, 100, 0], 500)
     else:
-        led.off()
+        #led.off()
+        pijuice.status.SetLedState('D2', [0, 0, 0])
     sleep(2)
---
+++ python_lab/pijuice_test.py
@@ -0,0 +1,6 @@
+from pijuice import PiJuice
+
+pijuice = PiJuice(1, 0x14)
+
+print(pijuice.status.SetLedState('D2', [0, 0, 0]))
+print(pijuice.status.SetLedBlink('D2', 10,[100, 0, 0], 500, [0, 100, 0], 500))
