ba443a469e7b2b76bf2225f756126c7cbcf01a9b
kpwang2005 <kpwang2005@gmail.com>
2019-02-23 20:40:18 +0800

    blink indefinite

--- python_lab/pijuice_test.py
+++ python_lab/pijuice_test.py
@@ -4,3 +4,4 @@ pijuice = PiJuice(1, 0x14)
 
 print(pijuice.status.SetLedState('D2', [0, 0, 0]))
 print(pijuice.status.SetLedBlink('D2', 10,[100, 0, 0], 500, [0, 100, 0], 500))
+# count=255 -> indefinite
